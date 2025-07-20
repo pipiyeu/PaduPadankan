0df21ff7f4506aa2a2981cefada336fae2698348
Luthfiyyah Nur Nafiah <luthfiyyahnn3073@gmail.com>
2025-07-20 07:20:12 +0700

    pwa

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import { registerSW } from 'virtual:pwa-register';
 
 
 createRoot(document.getElementById('root')).render(
@@ -10,3 +10,4 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+registerSW({ immediate: true });
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,49 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: [
+        "favicon.ico",
+        "apple-touch-icon.png",
+        "favicon-96x96.png",
+        "favicon.svg"
+      ],
+      manifest: {
+        name: "M2Outfit",
+        short_name: "M2Outfit",
+        description: "Aplikasi padu padan outfit-mu!",
+        theme_color: "#2E8B57",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "/icons/web-app-manifest-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/web-app-manifest-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/icons/web-app-manifest-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     host: true,
   },
